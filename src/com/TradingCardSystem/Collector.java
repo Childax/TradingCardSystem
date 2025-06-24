1cc4b67a2a07bce230ba9bad22e0c606e8bda37c
xenoghoul <xenoghoul03@gmail.com>
2025-06-24 17:23:12 +0800

    Update Collector.java

--- src/com/TradingCardSystem/Collector.java
+++ src/com/TradingCardSystem/Collector.java
@@ -122,6 +122,24 @@ public class Collector {
         binders.add(new Binder(name));
     }
 
+    public boolean removeBinder(String name) {
+        Binder binderToRemove = getBinderByName(name);
+
+        if(binderToRemove == null){
+            System.out.println("Binder with name \"" + name + "\" not found.");
+            return false;
+        }
+
+        for(Card card : binderToRemove.getCards()){
+            this.addCard(card);
+        }
+
+        binders.remove(binderToRemove);
+
+        System.out.println("Binder \"" + name + "\" has been removed and cards returned to your collection.");
+        return true;
+    }
+
     public Binder getBinderByName(String name) {
         for (Binder b : binders) {
             if (b.getName().equalsIgnoreCase(name)) {
@@ -140,4 +158,43 @@ public class Collector {
         return false;
     }
 
+    public void addDeck(String name) {
+        decks.add(new Deck(name));
+    }
+
+    public boolean removeDeck(String name) {
+        Deck deckToRemove = getDeckByName(name);
+
+        if (deckToRemove == null) {
+            System.out.println("Deck not found.");
+            return false;
+        }
+
+        for (Card card : deckToRemove.getCards()) {
+            this.addCard(card);
+        }
+
+        decks.remove(deckToRemove);
+
+        System.out.println("Deck '" + name + "' has been removed and cards returned to your collection.");
+        return true;
+    }
+
+    public Deck getDeckByName(String name) {
+        for (Deck d : decks) {
+            if(d.getDeckName().equalsIgnoreCase(name)) {
+                return d;
+            }
+        }
+        return null;
+    }
+
+    public boolean hasDeckWithName(String name) {
+        for (Deck d : decks) {
+            if (d.getDeckName().equalsIgnoreCase(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
