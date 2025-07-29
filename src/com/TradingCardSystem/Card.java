2b1882aab3b70bb4fb255db8a92c14887c903340
xenoghou1 <xenoghoul03@gmail.com>
2025-07-30 05:09:06 +0800

    javadox for extra card class methods

--- src/com/TradingCardSystem/Card.java
+++ src/com/TradingCardSystem/Card.java
@@ -256,14 +256,29 @@ public class Card {
         return sb.toString();
     }
 
+    /**
+     * Returns if card is rare or legendary
+     *
+     * @return true if card is rare or legendary
+     */
     public boolean isRareOrLegendary() {
         return getRarity() == Rarity.RARE || getRarity() == Rarity.LEGENDARY;
     }
 
+    /**
+     * Returns if card is common or uncommon
+     *
+     * @return true if card is common or uncommon
+     */
     public boolean isCommonOrUncommon() {
         return getRarity() == Rarity.COMMON || getRarity() == Rarity.UNCOMMON;
     }
 
+    /**
+     * Returns if card is rare or legendary
+     *
+     * @return true if card is normal variant
+     */
     public boolean isNormalVariant() {
         return getVariant() == Variant.NORMAL;
     }
